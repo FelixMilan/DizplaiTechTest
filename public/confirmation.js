34d8c9ad16577b9ba4898817131df74f9ccedd5e
Felix <felix.m.p.milan@gmail.com>
2024-01-26 15:48:42 +0000

    README added

--- public/confirmation.js
+++ public/confirmation.js
@@ -2,34 +2,36 @@ document.addEventListener('DOMContentLoaded', () => {
   const resultsContainer = document.getElementById('results');
 
   fetch('/api/votes/example-poll')
-      .then(response => response.json())
-      .then(votes => {
-          const allOptions = ['Manchester City', 'Arsenal', 'Liverpool'];
-          const allResults = allOptions.reduce((obj, option) => {
-              obj[option] = votes[option] || 0;
-              return obj;
-          }, {});
+  .then(response => response.json())
+  .then(votes => {
+    const allOptions = ['Manchester City', 'Arsenal', 'Liverpool'];
+    const allResults = allOptions.reduce((obj, option) => {
+      obj[option] = votes[option] || 0;
+      return obj;
+    }, {});
 
-          const sortedResults = Object.entries(allResults)
-              .sort(([, a], [, b]) => b - a)
-              .reduce((obj, [key, value]) => {
-                  obj[key] = value;
-                  return obj;
-              }, {});
+    const sortedResults = Object.entries(allResults)
+    .sort(([, a], [, b]) => b - a)
+    .reduce((obj, [key, value]) => {
+      obj[key] = value;
+      return obj;
+    }, {});
 
-          for (const option in sortedResults) {
-              const totalVotes = Object.values(allResults).reduce((a, b) => a + b, 0);
-              const percentage = totalVotes === 0 ? 0 : Math.round((sortedResults[option] / totalVotes) * 100);
-              const optionContainer = document.createElement('div');
-              
-              const resultItem = document.createElement('div');
-              resultItem.classList.add('result-item');
-              resultItem.textContent = `${option} ${percentage}%`;
+    for (const option in sortedResults) {
+      const totalVotes = Object.values(allResults).reduce((a, b) => a + b, 0);
+      const percentage = totalVotes === 0 ? 0 : Math.round((sortedResults[option] / totalVotes) * 100);
+      const optionContainer = document.createElement('div');   
+      const resultItem = document.createElement('div');
+      resultItem.classList.add('result-item');
+      resultItem.textContent = `${option} ${percentage}%`;
 
-              const color = `linear-gradient(90deg, #996BF9 ${percentage}%, rgb(140, 140, 140, 0.3) ${percentage}%)`;
-              resultItem.style.background = color;
-              optionContainer.appendChild(resultItem);
-              resultsContainer.appendChild(optionContainer);
-          }
-      });
+      const color = `linear-gradient(90deg, #996BF9 ${percentage}%, rgb(140, 140, 140, 0.3) ${percentage}%)`;
+      resultItem.style.background = color;
+      optionContainer.appendChild(resultItem);
+      resultsContainer.appendChild(optionContainer);
+    }
+    voteAgain.addEventListener('click', () => {
+      window.location.href = '/index.html';
+    });
+  });
 });
--- public/voting.js
+++ public/voting.js
@@ -1,39 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const optionsContainer = document.getElementById('options');
-    const voteButton = document.getElementById('voteButton');
-    const selectedOptionDisplay = document.getElementById('selectedOption');
+  const optionsContainer = document.getElementById('options');
+  const voteButton = document.getElementById('voteButton');
+  const selectedOptionDisplay = document.getElementById('selectedOption');
+  let selectedOption = null;
   
-    let selectedOption = null;
+  fetch('/api/polls/example-poll')
+  .then(response => response.json())
+  .then(poll => {
+    poll.options.forEach(option => {
+      const optionButton = document.createElement('button');
+      optionButton.textContent = option;
   
-    fetch('/api/polls/example-poll')
-      .then(response => response.json())
-      .then(poll => {
-        poll.options.forEach(option => {
-          const optionButton = document.createElement('button');
-          optionButton.textContent = option;
-  
-          optionButton.addEventListener('click', () => {
-            selectedOption = option;
-            voteButton.disabled = false;
-            selectedOptionDisplay.textContent = `Selected Option: ${option}`;
-          });
-  
-          optionsContainer.appendChild(optionButton);
-        });
-  
-        voteButton.addEventListener('click', () => {
-          fetch('/api/votes', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              pollId: 'example-poll',
-              selectedOption,
-            }),
-          });
-          window.location.href = '/confirmation.html';
-        });
+      optionButton.addEventListener('click', () => {
+        selectedOption = option;
+        voteButton.disabled = false;
+        selectedOptionDisplay.textContent = `Selected Option: ${option}`;
       });
+  
+        optionsContainer.appendChild(optionButton);
+    });
+  
+    voteButton.addEventListener('click', () => {
+      fetch('/api/votes', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          pollId: 'example-poll',
+          selectedOption,
+        }),
+      });
+      window.location.href = '/confirmation.html';
+    });
   });
+});
   
